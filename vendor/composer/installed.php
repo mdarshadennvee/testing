0aa2fd8ddc773e9644b9edd5e62a0c915c09b50f
Naveenkumarc8 <naveen.chinnasamy@ennvee.net>
2024-06-24 15:13:17 +0530

    addd ev

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'your-vendor-name/your-project-name',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9ea44902054c0ecd457e577ae700d60f12c6af19',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '3.10.1',
+            'version' => '3.10.1.0',
+            'reference' => '8f90f7a53ce271935282967f53d0894f8f1ff877',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+        'your-vendor-name/your-project-name' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9ea44902054c0ecd457e577ae700d60f12c6af19',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
