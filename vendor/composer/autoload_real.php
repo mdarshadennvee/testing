e68ddb5328df6433e462097891da0ad8c3748d7e
Naveenkumarc8 <naveen.chinnasamy@ennvee.net>
2024-06-25 10:57:34 +0530

    Psalm commit

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit0e500a0014af08a81734ca473ad475fa
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit0e500a0014af08a81734ca473ad475fa::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
